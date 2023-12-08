d9ef9f6dd2bf8d8ce31a1b9d1a731c8910fa1af7
Şerif Çam <serifcam34@gmail.com>
2023-12-08 22:27:39 +0300

    Boxing-Match
    boxing match developed by java.

---
+++ Boks/Main.java
@@ -0,0 +1,11 @@
+package Sinif.Boks;
+
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1=new Fighter("A",10,120,100,30);
+        Fighter f2=new Fighter("B",20,85,85,40);
+
+        Match match=new Match(f1,f2,80,100);
+        match.run();
+    }
+}
